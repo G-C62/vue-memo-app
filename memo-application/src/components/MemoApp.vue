4dbee22ad06f94d4864fdca8a47d5a0d9c560b1d
G-C62 <nsw9211@gmail.com>
2022-05-03 19:17:05 +0900

    feat: add Memo components
    - MemoApp
    - MemoForm
    - Memo
    - AppHeader

---
+++ memo-application/src/components/MemoApp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="memo-app">
+    <memo-form />
+    <memo />
+  </div>
+</template>
+<script>
+import MemoForm from './MemoForm.vue';
+import Memo from './Memo.vue';
+
+export default {
+  name: 'MemoApp',
+  data () {
+    return {
+      memos: [],
+    };
+  },
+  created () {
+    this.memos = localStorage.memos ? JSON.parse(localStorage.memos) : [];
+  }
+}
+</script>
